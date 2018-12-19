ab152ed03f0f8e2792156e9eb8380dc058a5ddc6
aaron-collier <aaron.collier@stanford.edu>
2018-12-19 14:47:51 -0800

    Rubocop cleanup

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ require 'action_controller/railtie'
 Bundler.require(*Rails.groups)
 
 module WorkflowServer
+  # Base Application
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
