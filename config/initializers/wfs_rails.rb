60f8aa9c18d3003759b605770c674fe2bd05e5b6
Justin Coyne <jcoyne@justincoyne.com>
2019-01-08 09:38:41 -0600

    Remove WfsRails initializer
    We decided that we were unable to use the legacy DB with this code,
    so there's no need to set the table name to the legacy table's name.

--- config/initializers/wfs_rails.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-# Change the name from the default 'wfs_rails_workflows' to what already
-# exists in the legacy Oracle database
-WfsRails::Workflow.table_name = 'workflow'
