a0e81bad4f30b3da55cdc5b779cbc5826997d941
Justin Coyne <jcoyne@justincoyne.com>
2019-01-10 11:31:57 -0600

    Database is now on the prod/stage machines

--- config/deploy/stage.rb
+++ config/deploy/stage.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
-server 'workflow-service-stage.stanford.edu', user: 'workflow', roles: %w[web app]
+server 'workflow-service-stage.stanford.edu', user: 'workflow', roles: %w[web app db]
 
 Capistrano::OneTimeKey.generate_one_time_key!
