8fc45683e933ca8dec9da243b2ce855358ed638e
Justin Coyne <jcoyne@justincoyne.com>
2019-01-14 11:59:56 -0600

    Only update or destroy the most current version

--- spec/controllers/workflows_controller_spec.rb
+++ spec/controllers/workflows_controller_spec.rb
@@ -5,10 +5,16 @@ require 'rails_helper'
 RSpec.describe WorkflowsController do
   include XmlFixtures
   let(:repository) { 'dor' }
+  let(:client) { double(current_version: '1') }
+  let(:wf) { FactoryBot.create(:workflow_step) }
+  let(:druid) { wf.druid }
+
+  before do
+    allow(Dor::Services::Client).to receive(:object).with(druid).and_return(client)
+  end
 
   describe 'GET lifecycle' do
     it 'loads ActiveRecord Relation and parses it to valid XML' do
-      wf = FactoryBot.create(:workflow_step)
       get :lifecycle, params: { repo: wf.repository, druid: wf.druid, format: :xml }
       expect(assigns(:objects)).to be_an ActiveRecord::Relation
       expect(assigns(:objects).count).to eq 1
@@ -17,7 +23,6 @@ RSpec.describe WorkflowsController do
   end
   describe 'GET index' do
     it 'loads and groups ActiveRecord Relation renders workflows' do
-      wf = FactoryBot.create(:workflow_step)
       get :index, params: { repo: wf.repository, druid: wf.druid, format: :xml }
       expect(assigns(:processes)).to be_an Hash
       expect(assigns(:processes).length).to eq 1
@@ -27,7 +32,6 @@ RSpec.describe WorkflowsController do
 
   describe 'GET show' do
     it 'loads and groups ActiveRecord Relation renders workflows' do
-      wf = FactoryBot.create(:workflow_step)
       get :show, params: { repo: wf.repository, druid: wf.druid, workflow: wf.datastream, format: :xml }
       expect(assigns(:processes)).to be_an Hash
       expect(assigns(:processes).length).to eq 1
@@ -37,7 +41,6 @@ RSpec.describe WorkflowsController do
 
   describe 'PUT update' do
     it 'updates the process to complete' do
-      wf = FactoryBot.create(:workflow_step)
       put :update, params: { repo: wf.repository, druid: wf.druid, workflow: wf.datastream, process: wf.process }
       expect(wf.reload.status).to eq 'completed'
       expect(response).to be_no_content
@@ -46,7 +49,6 @@ RSpec.describe WorkflowsController do
 
   describe 'GET archive' do
     it 'loads count of workflows' do
-      wf = FactoryBot.create(:workflow_step)
       get :archive, params: { repository: wf.repository, workflow: wf.datastream, format: :xml }
       expect(assigns(:objects)).to eq 1
       expect(response).to render_template 'archive'
@@ -54,8 +56,6 @@ RSpec.describe WorkflowsController do
   end
 
   describe 'DELETE destroy' do
-    let!(:wf) { FactoryBot.create(:workflow_step) }
-
     it 'deletes workflows' do
       delete :destroy, params: { repo: wf.repository, druid: wf.druid, workflow: wf.datastream, format: :xml }
       expect(response).to be_no_content
@@ -67,11 +67,7 @@ RSpec.describe WorkflowsController do
     let(:workflow) { 'accessionWF' }
     let(:repository) { 'dor' }
     let(:request_data) { workflow_create }
-    let(:client) { double(current_version: '1') }
 
-    before do
-      allow(Dor::Services::Client).to receive(:object).with(druid).and_return(client)
-    end
     it 'creates new workflows' do
       expect do
         put :create, body: request_data, params: { repo: repository, druid: druid, workflow: workflow, format: :xml }
