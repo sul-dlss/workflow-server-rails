8655db994fd9e8809ca4d4366df5235495c9d686
Justin Coyne <jcoyne@justincoyne.com>
2019-01-17 15:56:15 -0600

    Use Arel for building the intersections

--- app/services/intersect_query.rb
+++ app/services/intersect_query.rb
@@ -5,24 +5,27 @@
 class IntersectQuery
   class << self
     def intersect(scopes)
-      parts = []
-      binds = []
-      scopes.each do |scope|
-        inner_sql, inner_binds = to_sql_and_binds(scope)
-        parts << inner_sql
-        binds += inner_binds
-      end
-      [parts.join(' INTERSECT '), binds]
+      sql, binds = to_sql_and_binds build_intersect(scopes.map(&:arel))
+      # Remove the unneeded parenthesis. See https://github.com/rails/rails/pull/34437
+      [sql.gsub(/[()]/, ''), binds]
     end
 
     private
 
-    def to_sql_and_binds(scope)
+    def build_intersect(scopes)
+      if scopes.size == 2
+        Arel::Nodes::Intersect.new(scopes[0], scopes[1])
+      else
+        Arel::Nodes::Intersect.new(scopes[0], build_intersect(scopes[1..-1]))
+      end
+    end
+
+    def to_sql_and_binds(arel)
       collector = Arel::Collectors::Composite.new(
         Arel::Collectors::SQLString.new,
         Arel::Collectors::Bind.new
       )
-      WorkflowStep.connection.visitor.accept(scope.arel.ast, collector).value
+      WorkflowStep.connection.visitor.accept(arel, collector).value
     end
   end
 end
