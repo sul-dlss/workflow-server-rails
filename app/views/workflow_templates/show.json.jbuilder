7022716d19a846152725fa41f47eee1802271712
Justin Coyne <jcoyne@justincoyne.com>
2019-09-12 13:55:50 -0500

    Add label to the workflow_template output
    Argo will use this to draw the 'all workflows' page

--- app/views/workflow_templates/show.json.jbuilder
+++ app/views/workflow_templates/show.json.jbuilder
@@ -1,3 +1,3 @@
 # frozen_string_literal: true
 
-json.processes @processes, :name
+json.processes @processes, :name, :label
--- spec/requests/show_workflow_template_spec.rb
+++ spec/requests/show_workflow_template_spec.rb
@@ -6,11 +6,19 @@ RSpec.describe 'Show a workflow template', type: :request do
   it 'draws an empty set of milestones' do
     get '/workflow_templates/assemblyWF'
     json = JSON.parse(response.body)
-    expect(json['processes']).to eq [{ 'name' => 'start-assembly' },
-                                     { 'name' => 'content-metadata-create' },
-                                     { 'name' => 'jp2-create' },
-                                     { 'name' => 'checksum-compute' },
-                                     { 'name' => 'exif-collect' },
-                                     { 'name' => 'accessioning-initiate' }]
+    expect(json['processes']).to eq [
+      { 'label' => 'Initiate assembly of the object',
+        'name' => 'start-assembly' },
+      { 'label' => 'Create content-metadata from stub content metadata if it exists',
+        'name' => 'content-metadata-create' },
+      { 'label' => 'Create JP2 derivatives for any images in object',
+        'name' => 'jp2-create' },
+      { 'label' => 'Compute and compare checksums for any files referenced in contentMetadata',
+        'name' => 'checksum-compute' },
+      { 'label' => 'Calculate and add exif, mimetype, file size and other attributes to each file node in contentMetadata',
+        'name' => 'exif-collect' },
+      { 'label' => 'Initiate workspace and start common accessioning',
+        'name' => 'accessioning-initiate' }
+    ]
   end
 end
