fd0ce91f5f04e8b4ae488d77ed39da3ad0592726
Justin Coyne <jcoyne@justincoyne.com>
2019-09-13 10:31:05 -0500

    the success? predicate is deprecated.  Switch to successful?

--- spec/requests/workflows/show_step_spec.rb
+++ spec/requests/workflows/show_step_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe 'Show a workflow step for an object', type: :request do
     it 'clears the old error message, but preserves the lifecycle' do
       get "/dor/objects/#{step.druid}/workflows/#{step.workflow}/#{step.process}"
 
-      expect(response).to be_success
+      expect(response).to be_successful
       expect(response.body).to be_equivalent_to <<~XML
         <process version="1" note="" lifecycle="submitted" laneId="default" elapsed="" attempts="0" datetime="#{step.updated_at.to_time.iso8601}" status="error" name="start-accession" errorMessage="Bang!"/>
       XML
