ab44e515738eb5c70fe00f2a1c1d6121c309b7d6
Justin Coyne <jcoyne@justincoyne.com>
2019-11-25 14:56:48 -0600

    Add a callback for preservation ingest
    We will soon move SdrIngestTransfer to dor-services-app.  As this can take a long time, it will be performed in the background. This new workflow step will be able to log that that process completed successfully.

--- spec/services/initial_workflow_parser_spec.rb
+++ spec/services/initial_workflow_parser_spec.rb
@@ -38,7 +38,7 @@ RSpec.describe InitialWorkflowParser do
 
     it 'is a list of ProcessParsers' do
       expect(processes).to all be_instance_of ProcessParser
-      expect(processes.size).to eq 15
+      expect(processes.size).to eq 16
     end
   end
 end
--- spec/services/workflow_template_parser_spec.rb
+++ spec/services/workflow_template_parser_spec.rb
@@ -34,7 +34,7 @@ RSpec.describe WorkflowTemplateParser do
     subject(:processes) { wf_parser.processes }
 
     it 'returns a list of process structs' do
-      expect(processes.length).to eq 15
+      expect(processes.length).to eq 16
       expect(processes).to all(be_an_instance_of(WorkflowTemplateParser::Process))
     end
   end
