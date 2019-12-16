0d58ca781a17b39a96c44103adb49cd82b62a851
Justin Coyne <jcoyne@justincoyne.com>
2019-12-16 16:23:04 -0600

    Use rails 6 defaults

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module WorkflowServer
   # Base Application
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 6.0
 
     # Log to STDOUT
     unless Rails.env.test?
