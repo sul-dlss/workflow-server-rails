84574660ddabbc352ec7c1ba66e491b6ab178104
Tony Zanella <tony.zanella@gmail.com>
2020-11-02 14:21:01 -0800

    Spot check db for data loss

--- config/initializers/okcomputer.rb
+++ config/initializers/okcomputer.rb
@@ -1,3 +1,29 @@
 # frozen_string_literal: true
 
 OkComputer.mount_at = 'status'
+
+# spot check db for data loss
+class TablesHaveDataCheck < OkComputer::Check
+  def check
+    msg = [
+      WorkflowStep
+    ].map { |klass| table_check(klass) }.join(' ')
+    mark_message msg
+  end
+
+  private
+
+  # @return [String] message
+  def table_check(klass)
+    # has at least 1 record
+    return "#{klass.name} has data." if klass.any?
+
+    mark_failure
+    "#{klass.name} has no data."
+  rescue => e # rubocop:disable Style/RescueStandardError
+    mark_failure
+    "#{e.class.name} received: #{e.message}."
+  end
+end
+
+OkComputer::Registry.register 'feature-tables-have-data', TablesHaveDataCheck.new
