8d525bda0140c537f30c1ea56d1fef2759e2ea54
Peter Mangiafico <peter@mangiafico.org>
2023-02-03 11:55:07 -0800

    add HB checkin for cron job

--- config/schedule.rb
+++ config/schedule.rb
@@ -20,11 +20,15 @@
 # end
 
 # Learn more: http://github.com/javan/whenever
-job_type :no_warnings_runner, "cd :path && RUBYOPT='-W0' bin/rails runner -e :environment ':task' :output"
+job_type :no_warnings_runner,
+         "cd :path && RUBYOPT='-W0' bin/rails runner -e :environment ':task' :output && curl --silent https://api.honeybadger.io/v1/check_in/:check_in"
 
+# NOTE: this job uses HB checkins.
+# If you change the schedule, edit the HB checkin at https://app.honeybadger.io/projects/58890/check_ins
 every 60.minutes do
   # Currently running Monitor (notification only for stuck workflow steps) instead of Sweeper (notification and requeueing).
   # If monitoring alone isn't adequate (e.g., due to large numbers of Redis timeouts), we may want to re-enable the sweeper.
   # no_warnings_runner 'Sweeper.sweep'
+  set :check_in, Settings.honeybadger_checkins.workflow_monitor
   no_warnings_runner 'WorkflowMonitor.monitor'
 end
