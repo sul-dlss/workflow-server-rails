afc1cb047d0656b356a2ec092470e94a8d719f29
Justin Littman <justinlittman@stanford.edu>
2024-10-29 07:35:15 -0400

    Upgrade to Rails 7.2

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-gem 'rails', '~> 7.1.0'
+gem 'rails', '~> 7.2.0'
 
 gem 'bootsnap', '>= 1.1.0', require: false
 gem 'bunny', '~> 2.17'
--- config/boot.rb
+++ config/boot.rb
@@ -4,3 +4,7 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
+
+# We aren't using cookies in this application, so just generate a value at startup.
+require 'securerandom'
+ENV['SECRET_KEY_BASE'] ||= SecureRandom.hex(64)
